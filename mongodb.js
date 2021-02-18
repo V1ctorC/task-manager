5a3dfbf00e97468219a262f8b02593a5e9ebbcfb
Victor <victor.du.77@hotmail.fr>
2021-02-18 18:56:32 +0100

    Inserting Documents

--- mongodb.js
+++ mongodb.js
@@ -12,8 +12,49 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Victor',
-        age: 26
+    // db.collection('users').insertOne({
+    //     name: 'Victor',
+    //     age: 26
+    // }, (error, result) => {
+    //     if (error) {
+    //         console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Victor',
+    //         age: 26
+    //     }, {
+    //         name: 'Pierre',
+    //         age: 18
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         console.log('Unable to insert documents')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Task number 1',
+            completed: true
+        }, {
+            description: 'Task number 2',
+            completed: false
+        }, {
+            description: 'Task number 3',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            console.log('Enable to insert document')
+        }
+
+        console.log(result.ops)
     })
 })
