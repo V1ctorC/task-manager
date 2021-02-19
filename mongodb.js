1577b9722975f538a795715f344a7fd001d18958
Victor <victor.du.77@hotmail.fr>
2021-02-19 11:41:39 +0100

    Updating Documents

--- mongodb.js
+++ mongodb.js
@@ -11,31 +11,28 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne(
-    //     {
-    //         _id: new ObjectID('602eacba6dee0805129ac949')
-    //     }, (error, user) => {
-    //         if (error) {
-    //             console.log('Unable to find user')
-    //         }
+//    db.collection('users').updateOne({
+//         _id: new ObjectID('602ea80190dbba04177871fd')
+//     }, {
+//         $inc: {
+//             age: 1
+//         }
+//     }).then((result) => {
+//         console.log(result)
+//     }).catch((error) => {
+//         console.log(error)
+//     })
 
-    //         console.log(user)
-    //     }
-    // )
-
-    // db.collection('users').find({ name: 'Victor'}).toArray((error, result) => {
-    //     console.log(result)
-    // })
-
-    // db.collection('users').find({ name: 'Victor'}).count((error, count) => {
-    //     console.log(count)
-    // })
-
-    db.collection('tasks').findOne({ _id: new ObjectID("602eaa052eafde0464bd8469") }, (error, result) => {
-        console.log(result)
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result.modifiedCount)
+    }).catch((error) => {
+        console.log(error)
     })
 
-    db.collection('tasks').find({ completed: false}).toArray((error, result) => {
-        console.log(result)
-    })
 })
