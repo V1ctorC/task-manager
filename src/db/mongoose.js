0f94d45e5394ae4e8876e6284bd71495230eddcc
Victor <victor.du.77@hotmail.fr>
2021-02-19 14:14:37 +0100

    Creating a Mongoose Model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,22 +5,43 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String
+//     },
+//     age: {
+//         type: Number
+//     }
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
         type: String
     },
-    age: {
-        type: Number
+    completed: {
+        type: Boolean
     }
 })
 
-const me = new User({
-    name: 'Victor',
-    age: 'yolo'
+// const me = new User({
+//     name: 'Victor',
+//     age: 26
+// })
+
+const task = new Task({
+    description: 'Task number 1',
+    completed: false
 })
 
-me.save().then(() => {
-    console.log(me)
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error : ', error)
+// })
+
+task.save().then(() => {
+    console.log(task)
 }).catch((error) => {
     console.log('Error : ', error)
 })
+
