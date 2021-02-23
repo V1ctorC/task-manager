1baf482624350d20b3719a2a4f88a9f3e1e5fcf1
Victor <victor.du.77@hotmail.fr>
2021-02-23 19:31:19 +0100

    Resource Reading Endpoints: Part I

--- src/index.js
+++ src/index.js
@@ -18,6 +18,31 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((err) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            res.status(500).send()
+        } 
+        
+        res.send(user)
+    }).catch(() => {
+        res.status(404).send()
+    })
+
+    console.log(req.params) 
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
