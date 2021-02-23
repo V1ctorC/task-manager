edfe10f00eb73ef3ff5c2e14396cc61934ffe098
Victor <victor.du.77@hotmail.fr>
2021-02-23 19:03:15 +0100

    Resource Creation Endpoints: Part I

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,62 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Invalid password')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be positive')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '   Test   ',
-//     email: '  tEst@test.cOm   ',
-//     password: '  my-passpword ',
-//     age: 99
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error : ', error)
-// })
 
 const Task = mongoose.model('Task', {
     description: {
@@ -69,15 +16,3 @@ const Task = mongoose.model('Task', {
         default: false
     }
 })
-
-const task = new Task({
-    description: 'Task number 3',
-    completed: false
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error : ', error)
-})
-
