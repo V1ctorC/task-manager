ad80a7742fe82c257c84349906714d82b79c73d9
Victor <victor.du.77@hotmail.fr>
2021-02-25 19:03:23 +0100

    Async/Await: Part II

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('603542019eed7119d9930e5b', { age: 1 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({age : 1})
-}).then((result) => {
-    console.log(result )
-}).catch((err) => {
-    console.log(err)
+// User.findByIdAndUpdate('603542019eed7119d9930e5b', { age: 1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({age : 1})
+// }).then((result) => {
+//     console.log(result )
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('603542019eed7119d9930e5b', 3).then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
 })
--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('6032a36b23a37411398d0a99').then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('6032a36b23a37411398d0a99').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+const findTaskAndDelete = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed: false})
+    return count
+}
+
+findTaskAndDelete('6035457ee295f61b02867e54').then((count) => {
+    console.log(count)
 }).catch((err) => {
     console.log(err)
 })
