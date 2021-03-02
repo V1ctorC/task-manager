bba9237bda66ff3efd19c6dad846434b4a0aeeb5
Victor <victor.du.77@hotmail.fr>
2021-03-02 18:35:50 +0100

    Resource Deleting Endpoints

--- src/index.js
+++ src/index.js
@@ -70,6 +70,21 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -133,6 +148,23 @@ app.patch('/tasks/:id', async (req, res) => {
         res.status(400).send(error)
     }
 
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+
+    try {
+        
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+
+    } catch (error) {
+        res.status(400).send()
+    }
 })
 
 app.listen(port, () => {
