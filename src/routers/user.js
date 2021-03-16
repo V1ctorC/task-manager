a8bd238ddfed24cab726f0758c66a2f5e6779cb6
Victor <victor.du.77@hotmail.fr>
2021-03-16 18:43:43 +0100

    Serving up Files

--- src/routers/user.js
+++ src/routers/user.js
@@ -116,4 +116,21 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     res.send(req.user)
 })
 
+router.get('/users/:id/avatar', async (req, res) => {
+    
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+        
+    } catch (error) {
+        res.status(404).send()
+    }
+})
+
 module.exports = router
