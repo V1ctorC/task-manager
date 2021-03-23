5af6c8625ae7411865fc5e2476c8b1f5c224f66e
Victor <victor.du.77@hotmail.fr>
2021-03-23 19:15:28 +0100

    Testing Asynchronous Code

--- src/math.js
+++ src/math.js
@@ -8,8 +8,22 @@ const celsiusToFahrenheit = (temp) => {
     return (temp * 1.8) + 32
 }
 
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            if (a < 0 || b < 0) {
+                return reject('Must be not negative')
+            }
+
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit,
+    add
 }
