6a235363216af339489fb3ff23756d8209635050
Victor <victor.du.77@hotmail.fr>
2021-03-26 11:33:07 +0100

    Bonus: Extra Test Ideas - tasks

--- test/task.test.js
+++ test/task.test.js
@@ -38,3 +38,95 @@ test('Should not delete other users tasks', async () => {
     const task = await Task.findById(taskOne._id)
     expect(task).not.toBeNull()
 })
+
+test('Should not create task with invalid completed', async () => {
+    await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send({
+            completed: 'FakeField'
+        }).expect(400)
+})
+
+test('Should not update task with invalid description/completed', async () => {
+    await request(app)
+        .patch(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            completed: 'FakeField'
+        }).expect(400)
+})
+
+test('Should delete user task', async () => {
+    await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should fetch user task by id', async () => {
+    const response = await request(app)
+        .get(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body).not.toBeNull()
+})
+
+test('Should not fetch user task by id if unauthenticated', async () => {
+    const response = await request(app)
+        .get(`/tasks/${taskOne._id}`)
+        .send()
+        .expect(401)
+})
+
+test('Should not fetch other users task by id', async () => {
+    await request(app)
+        .get(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(500)
+})
+
+test('Should fetch only completed tasks', async () => {
+    const response = await request(app)
+        .get(`/tasks?completed=true`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toBe(1)
+})
+
+test('Should fetch only incompleted tasks', async () => {
+    const response = await request(app)
+        .get(`/tasks?completed=false`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toBe(1)
+})
+
+test('Should sort tasks by completed', async () => {
+    const response = await request(app)
+        .get(`/tasks?sortBy=completed:true`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body[0].completed).toEqual(false)
+    expect(response.body[1].completed).toEqual(true)
+})
+
+test('Should fetch page of tasks', async () => {
+    const response = await request(app)
+        .get(`/tasks?limit=1&skip=1`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toBe(1)
+})
